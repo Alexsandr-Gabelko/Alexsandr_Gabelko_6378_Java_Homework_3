11bb3aa8f2505e6b017d42bcc0cc064847c09a93
Alexandr_Gabelko <aleksandrgabelko@gmail.com>
2024-09-20 13:09:39 +0500

    Файлы на проверку

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,6 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -128,6 +126,5 @@ public class Main {
         for (FlowOfStudyGroups stream : sortedStreams) {
             System.out.println("Поток с количеством групп: " + stream.getStudentGroupList().size());
         }
-
     }
 }
